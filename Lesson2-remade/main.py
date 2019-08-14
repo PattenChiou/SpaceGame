642434aa9df66dffd3a1f0c898454d34bbfff637
Patten <patten1719@gmail.com>
2019-08-14 10:54:18 +0800

    Move Player to Player

--- Lesson2-remade/main.py
+++ Lesson2-remade/main.py
@@ -4,6 +4,7 @@ from Explosion import Explosion
 import pygame
 from Bullet import *
 from Meteor import *
+from Player import *
 
 # TODO Refactor 將參數統一放到另外一個檔案
 from Env import *
@@ -16,36 +17,7 @@ pygame.mixer.init()
 pygame.mixer.music.load(path.join(sound_dir, "bgm.mp3"))
 pygame.mixer.music.play(-1)
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        image = pygame.image.load(path.join(img_dir, "ship.png"))
-        self.image = pygame.transform.scale(image, (50, 30))
-        self.rect = self.image.get_rect()
-        self.rect.centerx = x
-        self.rect.centery = y
-        self.speedx = 8
-        self.speedy=8
-        self.shield=100
 
-    def update(self):
-        self.keyEventHandling()
-
-    def keyEventHandling(self):
-        keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            player.move(-self.speedx, 0)
-        if keystate[pygame.K_RIGHT]:
-            player.move(self.speedx, 0)
-        
-        if keystate[pygame.K_UP]:
-            player.move(0,-self.speedy)
-        if keystate[pygame.K_DOWN]:
-            player.move(0,self.speedy)
-
-    def move(self, dx, dy):
-        self.rect.x += dx
-        self.rect.y += dy
 
 
 
