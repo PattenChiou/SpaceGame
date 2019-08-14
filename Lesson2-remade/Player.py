f7ce484d5dce137d96d5d0e3913e2f7fc265fa7b
Patten <patten1719@gmail.com>
2019-08-14 15:16:34 +0800

    make exe

--- Lesson2-remade/Player.py
+++ Lesson2-remade/Player.py
@@ -9,9 +9,11 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = x
         self.rect.centery = y
-        self.speedx = 8
-        self.speedy=8
+        self.speedx = 0
+        self.speedy=0
         self.shield=100
+        self.ax=0
+        self.ay=0
 
     def update(self):
         self.keyEventHandling()
@@ -19,15 +21,22 @@ class Player(pygame.sprite.Sprite):
     def keyEventHandling(self):
         keystate = pygame.key.get_pressed()
         if keystate[pygame.K_LEFT]:
-            self.move(-self.speedx, 0)
+            self.ax=-0.1
+            #self.move(-self.speedx, 0)
         if keystate[pygame.K_RIGHT]:
-            self.move(self.speedx, 0)
-        
+            #self.move(self.speedx, 0)
+            self.ax=0.1
         if keystate[pygame.K_UP]:
-            self.move(0,-self.speedy)
+            #self.move(0,-self.speedy)
+            self.ay=-0.1
         if keystate[pygame.K_DOWN]:
-            self.move(0,self.speedy)
-
+            #self.move(0,self.speedy)
+            self.ay=0.1
+        self.speedx+=self.ax
+        self.speedy+=self.ay
+        self.ax=0
+        self.ay=0
+        self.move(self.speedx,self.speedy)
     def move(self, dx, dy):
         self.rect.x += dx
         self.rect.y += dy
